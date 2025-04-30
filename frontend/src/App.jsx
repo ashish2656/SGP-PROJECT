af45554ba71ee1671300b55ba40725f941aa0c6d
ashish2656 <ashishdodiya2656@gmail.com>
2025-05-01 00:33:05 +0530

    Fix case-sensitive import path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Navbar from "./components/shared/Navbar";
 import { createBrowserRouter } from "react-router-dom";
-import Login from "./components/auth/login";
+import Login from "./components/auth/Login";
 import Signup from "./components/auth/Signup";
 import Home from "./components/Home";
 import Jobs from "./components/Jobs";
